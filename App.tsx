f19c1f30f7adcfafa0462488b7d0c2747d95c541
Pedro Henrique <136542488+pedrohenriquesantossilva@users.noreply.github.com>
2026-02-12 21:57:59 -0300

    Salvamento dados
    salva nome e idade

--- App.tsx
+++ App.tsx
@@ -1,43 +1,104 @@
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
+  const [nome, setNome] = useState('');
+  const [idade, setIdade] = useState('');
+
+  const salvar = () => {
+    Alert.alert(
+    `Dados Salvos`,
+    `Nome: ${nome}\nIdade: ${idade}`
+    );
+    };
+
+
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.texto}>
-        Meu nome é Pedro
+        Escreva Seu nome
       </Text>
+
+      
 
       <TextInput
         style={styles.input}
         placeholder="Escreva seu nome"
+        value={nome}
+        onChangeText={setNome}
       />
 
-      <Button title = "Salvar"/>
+      <Text style={styles.texto1}>
+        Digite Sua Idade
+      </Text>
+
+      <TextInput
+        style={styles.input1}
+        placeholder= "Digite Sua Idade"
+        value={idade}
+        onChangeText={setIdade}
+      />
+
+      <Button title = "Salvar"
+        onPress= {salvar}
+      />
         
     </View>
   );
 }
 
+
+
+
+
+
 const styles = StyleSheet.create({
   texto: {
-    fontSize: 30,
-    color: '#FF2900',
+    fontSize: 20,
+    color: 'blue',
     textAlign: 'center',
   },
+
+  texto1: {
+    fontSize: 20,
+    color: 'blue',
+    textAlign: 'center',
+  },
+
+
+
+
 
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#000000", 
+    backgroundColor: "#836FFF", 
   },
 
   input: {
-    backgroundColor: "#C7C7C7",
+    backgroundColor: "white",
     textAlign: "center",
     width: "80%",
     borderRadius: 4,
     height:"5%",
-    color: "red"
+    color: "#89CFF0",
+    marginTop: "10px",
+    marginBottom: "20px",
+  } ,
+
+  input1:{
+    backgroundColor: "white",
+    textAlign: "center",
+    width: "80%",
+    borderRadius: 4,
+    height:"5%",
+    color: "#89CFF0",
+    marginTop: "10px",
+    marginBottom: "20px",
   }
+  
+  
 });
